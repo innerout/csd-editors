56be9fee5fd3a99974e5d62c590212c06924b2df
George Xanthakis <kompiouterakias@hotmail.com>
2022-03-14 14:00:16 +0000

    Fix typo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo -i
 apt install emacs unzip bear fuse clang gdb python3-pip npm ripgrep fd-find
 npm install -g yarn
 
-# We need this since debian use older versiosn that are not compatible with our configuration
+# We need this since debian use older versions that are not compatible with our configuration
 wget https://github.com/neovim/neovim/releases/download/v0.6.1/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage /usr/bin/nvim
