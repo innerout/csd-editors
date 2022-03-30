8215f0f9d5735f1a54bff9ee5979f98b12e9dc37
George Xanthakis <kompiouterakias@hotmail.com>
2022-03-30 23:40:07 +0300

    Update lunarvim config

--- config.lua
+++ config.lua
@@ -54,7 +54,9 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
@@ -65,6 +67,7 @@ lvim.builtin.treesitter.ensure_installed = {
   "c",
 }
 
+lvim.lsp.automatic_servers_installation = true
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 require("lvim.lsp.manager").setup("sumneko_lua")
